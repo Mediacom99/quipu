1a6b00c8e98190e031502fb6393e873a5555c972
Mediacom <bertoliedoardo99@gmail.com>
2024-09-15 18:17:40 +0200

    Added logutil module as wrapper around tracing and tracing-subscriber setup.

---
+++ src/logutil.rs
@@ -0,0 +1,41 @@
+//! Utility wrapper functions around `tracing` and `tracing-subscriber` crates
+use tracing_subscriber::filter::EnvFilter;
+use tracing::*;
+
+/// Fast tracing subscriber setup with RUST_LOG env variable
+/// filter.
+pub async fn tracing_subscriber_setup(log_level: &str) {
+
+    //TODO check if user already set it
+    std::env::set_var("RUST_LOG", log_level);
+        
+    //Construct a format for a subscriber
+    let format = tracing_subscriber::fmt::format()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .pretty();
+
+    //control with default env RUST_LOG
+    //like RUST_LOG=debug cargo run
+    let filter = EnvFilter::from_default_env();
+
+    tracing_subscriber::fmt()
+        .event_format(format)
+        .with_env_filter(filter)
+        .init();
+
+}
+
+///Stupid function to test different tracing event macros
+#[instrument(level = "trace")] //add tracing span to this function
+pub async fn try_event_macros() {
+    println!("Do smth");
+    trace!("This is a trace");
+    debug!("This is a debug");
+    info!("This is an info");
+    warn!("This is a warning");
+    error!("This is an error")
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-fn main(){ 
+//Log utilities for tracing and tracing-sub
+mod logutil;
 
-    println!("Hello World!");
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    logutil::tracing_subscriber_setup("trace").await;
+
+
+    Ok(())
 }
+
